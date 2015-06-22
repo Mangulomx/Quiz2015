a2f9477c7ce4f6aa038ca3ca6781ac23091316a4
Mangulo <mangulomx@gmail.com>
2015-06-22 15:49:30 +0200

    Añadiendo el modelo al proyecto

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,23 +1,31 @@
+
+var models = require('../models/models.js');
+
 //GET /quizes/question
 
 exports.question = function(req, res)
 {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz)
+	{
+		res.render('quizes/question', quiz[0].pregunta);
+	})
 };
 
 //GET /quizes/answer
 
 exports.answer = function(req, res)
 {
-	if(req.query.respuesta === 'Roma')
+	models.Quiz.findAll().success(function(quiz)
 	{
-		res.render('quizes/answer', {respuesta: 'Correcta'});
-	}
-	else
-	{
-		res.render('quizes/answer', {respuesta: 'Incorrecta'});
-	}
-
+		if(req.query.respuesta === quiz[0].respuesta)
+		{
+			res.render('quizes/answer', {respuesta: 'Correcta'});
+		}
+		else
+		{
+			res.render('quizes/answer', {respuesta: 'Incorrecta'});
+		}
+	})
 };
 
 //GET /author
---
+++ models/models.js
@@ -0,0 +1,28 @@
+var path = require('path');
+//Cargar Modelo ORM
+var Sequelize = require('sequelize');
+//Usar 8800 SQLite
+
+var sequelize = new Sequelize(null, null, null, {dialect:"sqlite", storage:"quiz.sqlite"});
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz = Quiz; //exporta la definición de la tabla Quiz
+//sequelize.syncr() crea e inicializa la tabla de preguntas en la DB
+sequelize.sync().success(function(){
+	//success(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().sucess(function(count)
+	{
+		if(count === 0) //La tabla se inicializa si esta vacia
+		{
+			Quiz.create(
+			{
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			})
+			.success(function()
+			{
+				console.log('Base de datos inicializada');
+			});
+		}
+	});
+});
