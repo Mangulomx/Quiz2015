73fd2f977e39a52e904d5fce66a83ec4d41dc3e2
Mangulo <mangulomx@gmail.com>
2015-07-05 12:41:11 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -64,3 +64,27 @@ exports.answer = function(req, res)
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
+//GET /quizes/new
+
+exports.new = function(req, res)
+{
+	var quiz = models.Quiz.build(
+		//Crea objeto
+		{pregunta:"Pregunta", respuesta:"Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+
+exports.create = function(req, res)
+{
+	var quiz = models.Quiz.build(req.body.quiz);
+	//Guardar en la DB los campos pregunta y respuesta de quiz
+
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function()
+	{
+		res.redirect('/quizes'); //Redirect http(url relativo) lista de preguntas.
+	})
+};
+
