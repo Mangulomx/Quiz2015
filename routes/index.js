8f6a8d30e0393c2924012d57ededb832e42b185e
Mangulo <mangulomx@gmail.com>
2015-07-12 18:46:52 +0200

    Autentificación y sesión

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 // Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: "Quiz", errors: [] });
@@ -15,6 +16,10 @@ router.get('/author', function(req, res)
 });
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+//Definicion de rutas de sesion
+router.get('/login', sessionController.new); //formulario login
+router.post('/login', sessionController.create); //crear sesión
+router.get('/logout', sessionController.destroy); //destruir la sesión
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
